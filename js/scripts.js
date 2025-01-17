3813d46815966fb29a24176cf856153a83280648
Cristiano Israel <54605589+cristianotutu@users.noreply.github.com>
2025-01-17 01:57:09 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-
 // scripts.js
 document.addEventListener('DOMContentLoaded', () => {
   // Widget Container Content
   const widgetContainer = document.getElementById('widget-container');
-  widgetContainer.innerHTML = '<p>Widget carregado com sucesso!</p>';
+  widgetContainer.innerHTML = '<p>Bem-vindo ao widget CSI2! Aqui você encontra soluções de segurança.</p>';
 
   // Console log for debugging
-  console.log('Site TEC está funcionando perfeitamente!');
+  console.log('Site CSI2 está funcionando perfeitamente!');
 });
